ddf46ae95592d15edf218291a4f3cfc6d55498ff
Cristian Sovan <cristian.sovan@nagarro.com>
2023-07-14 12:39:57 +0300

    firebase cloudDB connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection, addDoc, doc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IntClinic';
+
+  userData!: Observable<any>;
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+  addData(f: any) {
+    const testCollection = collection(this.firestore, 'Users');
+    addDoc(testCollection, f.value)
+      .then(() => {
+        console.log('Data saved');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  getData() {
+    const testCollection = collection(this.firestore, 'Users');
+
+    collectionData(testCollection).subscribe(val => {
+      console.log(val);
+    });
+
+    this.userData = collectionData(testCollection, { idField: 'id' });
+  }
+
+  updateData(id: string) {
+    const docInstance = doc(this.firestore, 'Users', id);
+    const updateData = {
+      name: 'updatedName',
+    };
+
+    updateDoc(docInstance, updateData)
+      .then(() => {
+        console.log('Data Changeds');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  deleteData(id: string) {
+    const docInstance = doc(this.firestore, 'Users', id);
+    deleteDoc(docInstance).then(() => {
+      console.log('Data deleted');
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'int-clinic',
+    appId: '1:981743238567:web:4d762ad80a0a1aef760a82',
+    storageBucket: 'int-clinic.appspot.com',
+    apiKey: 'AIzaSyDT6naFYtKTQupxrzWdb315TvM00aOpZvE',
+    authDomain: 'int-clinic.firebaseapp.com',
+    messagingSenderId: '981743238567',
+  },
+};
