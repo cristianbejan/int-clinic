976dd75abd26e81e43630f412c45aceea2719a53
Leonard Constantin <leonard.constantin@nagarro.com>
2023-08-02 16:58:59 +0300

    Pull request #26: StepperChooseClinic
    Merge in INT/craiova-internship from stepperChooseClinic to firebase-test
    
    Squashed commit of the following:
    
    commit 4818ab5627c0722aaf8d08e5745db81370a6f176
    Author: Leonard Constantin <leonard.constantin@nagarro.com>
    Date:   Wed Aug 2 16:58:02 2023 +0300
    
        deleted formGroups from patient-appointment component
    
    commit 9daafa383d6463f4e35cd904ca6fd7ea85d9a649
    Author: Leonard Constantin <leonard.constantin@nagarro.com>
    Date:   Wed Aug 2 16:49:59 2023 +0300
    
        added search input and improved some logic

--- src/app/core/modules/patient/components/patient-appointment/choose-clinic/choose-clinic.component.ts
+++ src/app/core/modules/patient/components/patient-appointment/choose-clinic/choose-clinic.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { combineLatest } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { combineLatest, filter } from 'rxjs';
 import { Appointment } from 'src/app/core/interfaces/appointment.interface';
 import { Clinic } from 'src/app/core/interfaces/clinic.interface';
 import { ClinicService } from 'src/app/core/services/clinic.service';
@@ -12,10 +11,9 @@ import { DataStoreService } from 'src/app/core/services/data-store.service';
   styleUrls: ['./choose-clinic.component.scss'],
 })
 export class ChooseClinicComponent implements OnInit {
-  clinics!: Clinic[];
+  clinics: Clinic[] = [];
+  appointmentData!: Appointment;
   selectedClinic!: Clinic;
-  receivedAppointment!: Appointment;
-  updatedAppointment!: Appointment;
 
   constructor(
     private clinicService: ClinicService,
@@ -27,29 +25,30 @@ export class ChooseClinicComponent implements OnInit {
   }
 
   getClinics() {
+    console.log('get clinics');
+
     combineLatest([this.dataStoreService.appointmentDetails])
       .pipe(filter(([data]) => !!data))
       .subscribe(([data]) => {
-        this.receivedAppointment = data;
+        this.appointmentData = data;
 
         this.clinicService.getClinics().subscribe(clinics => {
-          this.clinics = clinics.filter(clinic => clinic.specialtyIds?.includes(this.receivedAppointment.specialtyId));
+          this.clinics = clinics.filter(clinic => clinic.specialtyIds?.includes(this.appointmentData.specialtyId));
         });
       });
   }
 
-  isActive(item: Clinic) {
-    return this.selectedClinic === item;
+  pickClinic(clinic: Clinic) {
+    this.selectedClinic = clinic;
+    this.sendPickedClinic();
   }
 
-  pickedClinic(clinic: Clinic) {
-    this.selectedClinic = clinic;
-    if (clinic.id) {
-      this.updatedAppointment = {
-        ...this.receivedAppointment,
-        clinicId: clinic.id,
-      };
-    }
-    this.dataStoreService.addData(this.updatedAppointment);
+  sendPickedClinic() {
+    const newData = { ...this.appointmentData, clinicId: this.selectedClinic.id };
+    this.dataStoreService.addData(newData);
+  }
+
+  isActive(item: Clinic) {
+    return this.selectedClinic && this.selectedClinic.id === item.id;
   }
 }
--- src/app/core/modules/patient/components/patient-appointment/patient-appointment.component.ts
+++ src/app/core/modules/patient/components/patient-appointment/patient-appointment.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-
 @Component({
   selector: 'app-patient-appointment',
   templateUrl: './patient-appointment.component.html',
   styleUrls: ['./patient-appointment.component.scss'],
 })
-export class PatientAppointmentComponent {
-  constructor(private _formBuilder: FormBuilder) {}
-  firstFormGroup: FormGroup = this._formBuilder.group({ firstCtrl: [''] });
-  secondFormGroup: FormGroup = this._formBuilder.group({ secondCtrl: [''] });
-}
+export class PatientAppointmentComponent {}
