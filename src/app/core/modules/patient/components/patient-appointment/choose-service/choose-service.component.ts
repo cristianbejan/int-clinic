72608e2e65f211434f35d7fad1eecf14fb713ce2
Daniel Grigorie <daniel.grigorie@nagarro.com>
2023-08-02 17:15:45 +0300

    Pull request #29: Patient specialty
    Merge in INT/craiova-internship from patient-specialty to firebase-test
    
    Squashed commit of the following:
    
    commit f68768b30368b7967eb4f93db80ba952b0aa555b
    Merge: a409c72 5cc2286
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 17:15:21 2023 +0300
    
        final merged patient-specialty with firebase-test
    
    commit a409c72f78db5123e458f22ba1bed43ce5babe6a
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 17:11:52 2023 +0300
    
        fixed patient-service component
    
    commit cf2dcf1f68fa316e95d5a3dfb6a12bcbf8296352
    Merge: 7355099 ed02e15
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 17:05:30 2023 +0300
    
        merged patient-specialty with firebase-test
    
    commit 7355099e40946a3440c8104661d3f3659b44554d
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 16:48:22 2023 +0300
    
        implemented-filterBy-pipe-for-patient
    
    commit e7a844e1acddc567939ed9f85542b72e34fa02c4
    Merge: 63cec76 1c95d00
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 10:52:20 2023 +0300
    
        merged patient-spacialty with firebase-test
    
    commit 63cec76ac991e31e4bf5634ca2dca35b9ffb5a1e
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 10:49:41 2023 +0300
    
        removed deselect-functionality
    
    commit d8b542c37bd5b8303b59d97b09130d1752a73f45
    Merge: c1f9a80 a39643c
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 10:32:48 2023 +0300
    
        Merge branch 'firebase-test' of ssh://git-niq.nagarro.com/int/craiova-internship into patient-specialty
    
    commit c1f9a808b234754be4fca9734122842e956e8853
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Wed Aug 2 10:32:38 2023 +0300
    
        completed choose-service&choose-specialty component
    
    commit bf4cba5c2413a4c0774413a2870bf448ccbf2d59
    Author: Daniel Grigorie <daniel.grigorie@nagarro.com>
    Date:   Tue Aug 1 16:52:48 2023 +0300
    
        implemented-patient-specialty-card-selection

--- src/app/core/modules/patient/components/patient-appointment/choose-service/choose-service.component.ts
+++ src/app/core/modules/patient/components/patient-appointment/choose-service/choose-service.component.ts
@@ -1,8 +1,56 @@
 import { Component } from '@angular/core';
+import { Appointment } from 'src/app/core/interfaces/appointment.interface';
+import { Services } from 'src/app/core/interfaces/services.interface';
+import { Specialty } from 'src/app/core/interfaces/specialty.interface';
+import { DataStoreService } from 'src/app/core/services/data-store.service';
+import { ServicesService } from 'src/app/core/services/services.service';
+import { SpecialtiesService } from 'src/app/core/services/specialties.service';
 
 @Component({
   selector: 'app-choose-service',
   templateUrl: './choose-service.component.html',
   styleUrls: ['./choose-service.component.scss'],
 })
-export class ChooseServiceComponent {}
+export class ChooseServiceComponent {
+  services: Services[] = [];
+  selected!: Services;
+  specialtyOption!: Specialty;
+  isTrue = false;
+  searchedInput = '';
+  receivedAppointment!: Appointment;
+
+  constructor(
+    private servicesService: ServicesService,
+    private specialtyService: SpecialtiesService,
+    private dataStoreService: DataStoreService
+  ) {
+    this.servicesService.getServices().subscribe(services => (this.services = services as Services[]));
+    this.dataStoreService.appointmentDetails.subscribe(data => {
+      this.receivedAppointment = data;
+      if (data.specialtyId === '') {
+        return;
+      }
+      this.specialtyService.getSpecialty(data.specialtyId).subscribe(specialty => {
+        this.specialtyOption = specialty['data']();
+        this.services = this.services.filter(service => this.specialtyOption.serviceIds.includes(service.id));
+      });
+    });
+  }
+
+  pickedService(service: Services) {
+    this.isTrue = !this.isTrue;
+    this.selected = service;
+
+    this.sendPickedService();
+  }
+
+  sendPickedService() {
+    const data: Appointment = { ...this.receivedAppointment, serviceId: this.selected.id };
+
+    this.dataStoreService.addData(data);
+  }
+
+  isActive(item: Services) {
+    return this.selected === item;
+  }
+}
