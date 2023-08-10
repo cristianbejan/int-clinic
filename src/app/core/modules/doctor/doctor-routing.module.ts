ee6e0800ca5fd98f982747427b48c493ffffc806
Daniel Grigorie <daniel.grigorie@nagarro.com>
2023-08-10 11:41:45 +0300

    added routes of appoints and filter

--- src/app/core/modules/doctor/doctor-routing.module.ts
+++ src/app/core/modules/doctor/doctor-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DoctorComponent } from './components/doctor.component';
+import { DoctorAppointmentsComponent } from './components/doctor-appointments/doctor-appointments.component';
+import { DoctorFilterComponent } from './components/doctor-filter/doctor-filter.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DoctorComponent,
-    children: [],
+    children: [
+      {
+        path: 'appointments',
+        component: DoctorAppointmentsComponent,
+        children: [
+          {
+            path: 'filter',
+            component: DoctorFilterComponent,
+            children: [],
+          },
+        ],
+      },
+    ],
   },
 ];
 
